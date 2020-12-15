25deb872a2fc5070386506d32319e12fb5850684
NICCOLO' CARDELLI <n.cardelli2@studenti.unipi.it>
2020-12-15 17:28:23 +0100

    cosine_distance test

--- sparselsh/lsh.py
+++ sparselsh/lsh.py
@@ -326,4 +326,4 @@ class LSH(object):
         # x_n = csr_matrix.sqrt(csr_matrix.dot(x, x.T))
         # y_n = csr_matrix.sqrt(csr_matrix.dot(y, y.T))
         # return 1 - csr_matrix.dot(x, y.T) / (x_n * y_n)
-        return cosine(x, y)
+        return cosine(x.toarray(), y.toarray())
